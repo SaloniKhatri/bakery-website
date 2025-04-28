c7d434c80ad3b9c6e6ad2eb182ee8e9aba46aa0c
SaloniKhatri <122539374+SaloniKhatri@users.noreply.github.com>
2025-04-28 19:19:12 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,101 @@
+// import React from 'react';
+// import './App.css';
+// import Navbar from './Components/Navbar';
+// import Home from './Components/Home';
+// import Menu from './Components/Menu'
+// import About from './Components/About';
+// import Gallery from './Components/Gallery';
+// import Contact from './Components/Contact';
+// import Cart from './Components/Cart';
+// import LogIn from './Components/LogIn';
+// import { createBrowserRouter, RouterProvider, useNavigate } from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min'; // Includes collapse functionality
+
+
+
+// const router = createBrowserRouter(
+//         [
+//                 {
+//                         path: "/",
+//                         element:
+//                                 <div>
+//                                         <Navbar />
+//                                         <Home />
+//                                 </div>
+//                 },
+//                 {
+//                         path: "/menu",
+//                         element:
+//                                 <div>
+//                                         <Navbar />
+//                                         <Menu />
+//                                 </div>
+//                 },
+//                 {
+//                         path: "/about",
+//                         element:
+//                                 <div>
+//                                         <Navbar />
+//                                         <About />
+//                                 </div>
+//                 },
+//                 {
+//                         path: "/gallery",
+//                         element:
+//                                 <div>
+//                                         <Navbar />
+//                                         <Gallery />
+//                                 </div>
+//                 },
+//                 {
+//                         path: "/contact",
+//                         element:
+//                                 <div>
+//                                         <Navbar />
+//                                         <Contact />
+//                                 </div>
+//                 },
+//                 {
+//                         path: "/cart",
+//                         element:
+
+//                         <div>
+//                                  <Navbar />
+//                                  <Cart/>
+//                         </div>
+
+//                 },
+//                 {
+//                         path: "/login",
+//                         element:
+
+//                         <div>
+//                                  <Navbar />
+//                                  <LogIn/>
+
+//                         </div>
+
+//                 },
+//         ]
+// );
+
+
+// function App() {
+//         return (
+//                 <div>
+//                         <RouterProvider router={router} />
+//                 </div>
+//         )
+// }
+// export default App;
+
+
+//3js   
+
 import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Menu from './Components/Menu'
@@ -8,87 +104,26 @@ import Gallery from './Components/Gallery';
 import Contact from './Components/Contact';
 import Cart from './Components/Cart';
 import LogIn from './Components/LogIn';
-import { createBrowserRouter, RouterProvider, useNavigate } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min'; // Includes collapse functionality
-
-
-
-const router = createBrowserRouter(
-        [
-                {
-                        path: "/",
-                        element:
-                                <div>
-                                        <Navbar />
-                                        <Home />
-                                </div>
-                },
-                {
-                        path: "/menu",
-                        element:
-                                <div>
-                                        <Navbar />
-                                        <Menu />
-                                </div>
-                },
-                {
-                        path: "/about",
-                        element:
-                                <div>
-                                        <Navbar />
-                                        <About />
-                                </div>
-                },
-                {
-                        path: "/gallery",
-                        element:
-                                <div>
-                                        <Navbar />
-                                        <Gallery />
-                                </div>
-                },
-                {
-                        path: "/contact",
-                        element:
-                                <div>
-                                        <Navbar />
-                                        <Contact />
-                                </div>
-                },
-                {
-                        path: "/cart",
-                        element:
-
-                        <div>
-                                 <Navbar />
-                                 <Cart/>
-                        </div>
-                        
-                },
-                {
-                        path: "/login",
-                        element:
-
-                        <div>
-                                 <Navbar />
-                                 <LogIn/>
-                                 
-                        </div>
-                        
-                },
-        ]
-);
 
 
 function App() {
         return (
-                <div>
-                        <RouterProvider router={router} />
-                </div>
-        )
+                <>
+                <Navbar />
+                <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/menu" element={<Menu />} />
+                        <Route path="/gallery" element={<Gallery />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/Login" element={<LogIn />} />
+                        
+                </Routes>
+                
+                </>
+               
+        );
 }
+
 export default App;
-
-
-//3js   
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bakery-website',
-  
+  base: process.env.NODE_ENV === 'production' ? '/Bakery-website/' : '/',
 })
